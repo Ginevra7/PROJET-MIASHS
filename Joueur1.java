c7ef4debc1bbadb40cdc945d52b11d2a3e644e24
emarti60 <60789525+emarti60@users.noreply.github.com>
2020-03-20 21:58:42 +0100

    Update Joueur1.java

--- Joueur1.java
+++ Joueur1.java
@@ -1,40 +1,47 @@
 package projetjeu;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
-
+import java.net.UnknownHostException;
+import java.util.*;
 public class Joueur1 {
-static final int port = 6112 ;
-	
-	public static void main(String[] args) throws Exception{
-		Socket socket = new Socket ("127.0.0.1", port);
-		System.out.println("SOCKET+" + socket);
-		
-		BufferedReader plec = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-		
-		PrintWriter pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
-				true);
-		
-		Scanner scan = new Scanner (System.in);
-		for (int i =0; i<10; i++) {
-			String s=scan.nextLine();
-			pred.println(s);
-			String str = plec.readLine();
-			System.out.println("Joueur1 recoit:" +str);
+	public static final int port=1180;
+	public static void main(String [] args)throws Exception{
+		Socket socket=null;
+
+		try{
+		socket=new Socket("127.0.0.1",port);
+		System.out.println("SOCKET="+ socket);
 		}
-		
-		System.out.println("Fin Joueur1");
+		catch(UnknownHostException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		finally{
+			if(socket!=null){
+				try{
+					socket.close();
+				}catch(IOException e){
+					e.printStackTrace();
+					socket=null;
+					}
+			}
+		}
+		BufferedReader plec=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter pred=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+		Scanner scanner=new Scanner(System.in);
+		for(int i=0;i<5;i++){
+		String s=scanner.nextLine();
+		pred.println(s);
+		String str=plec.readLine();
+		System.out.println("Joueur1 reçoit:" +str);
+		System.out.println("FIN JOUEUR1");
 		pred.println("END");
-		plec.close();
-		pred.close();
-		socket.close();
+		scanner.close();
+}
+}
 }
 
-}
 
 
