c9df95d1ab7437f0bce37dfa1ed7bede3b98dc29
Ginevra7 <60789454+Ginevra7@users.noreply.github.com>
2020-04-22 16:15:13 +0200

    Delete TestSuppression.java

--- TestSuppression.java
+++
@@ -1,9 +0,0 @@
-package projetjeu;
-//TEST DE SUPRESSION QUI NE FONCTIONNE PAS 
-public class TestSuppression {
-	public static void main(String[]args) {
-	Suppression a =  new Suppression("Solène tchatche");
-	System.out.println(a.supprimer());
-
-}
-}
