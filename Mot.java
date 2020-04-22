105e7637b2e85ed3ee92b67559c2d048cc4d90fa
Ginevra7 <60789454+Ginevra7@users.noreply.github.com>
2020-04-22 16:20:19 +0200

    Add files via upload

---
+++ Mot.java
@@ -0,0 +1,54 @@
+package socket;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+//Chaque instance de cette classe crée un objet composé d'une string (le mot) et une liste de string (les synonymes du mot) 
+
+public class Mot {
+	private String mot;
+	private ArrayList<String> synonymes;
+	public Mot(String mot){
+		setMot(mot);
+		synonymes=new ArrayList <String>();
+	}
+	public void setMot(String mot){
+		if(mot.length()>2)
+			this.mot=mot;
+		else 
+			System.out.println("Le mot n'est pas valide");
+	}
+	
+	public String getMot(){
+		return mot;
+	}
+	
+	public void ajoutSyno(String syno){
+		synonymes.add(syno);
+	}
+	
+	public String getSyno(int i){
+		return synonymes.get(i);
+	}
+	
+	public int nbSyno(){
+		return synonymes.size();
+	}
+	
+	//Cette méthode est utilisée par la méthode isSyno de la classe Dictionnaire et compare le mot d'une phrase du joueur1 avec tous les synonymes du mot but du jeux  
+	public Boolean isSyno(String str){ 
+		String s="";
+		Iterator <String> i=synonymes.iterator(); 
+		while(i.hasNext()){
+			{
+				s+=i.next();
+				if(str.equals(s))
+					return true;
+			}
+			s="";
+				
+		}
+		return false;
+	}		
+}
+
+
